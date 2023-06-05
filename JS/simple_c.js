04d87277e5256798f65e897fbfcb1f688f249d5d
Bittu kumar <bk2671111@gmail.com>
2023-06-05 14:09:54 +0530

    second3 commit

--- JS/simple_c.js
+++ JS/simple_c.js
@@ -5,7 +5,8 @@ let clear = document.getElementsByClassName("screen");
 
 const sum = () => {
   result = parseInt(firstnum.value) + parseInt(secondnum.value);
-  document.getElementById("result").innerHTML =
+  document.getElementById("result").innerHTML = result;
+  document.getElementById("mini-result").innerHTML =
     firstnum.value + " + " + secondnum.value + " = " + result;
   firstnum.value = "";
   secondnum.value = "";
@@ -13,24 +14,27 @@ const sum = () => {
 
 const sub = () => {
     result = parseInt(firstnum.value) - parseInt(secondnum.value);
-    document.getElementById("result").innerHTML =
-      firstnum.value + " - " + secondnum.value + " = " + result;
+    document.getElementById("result").innerHTML = result;
+    document.getElementById("mini-result").innerHTML =
+    firstnum.value + " + " + secondnum.value + " = " + result;
     firstnum.value = "";
     secondnum.value = "";
   };
 
   const mul = () => {
     result = parseInt(firstnum.value) * parseInt(secondnum.value);
-    document.getElementById("result").innerHTML =
-      firstnum.value + " * " + secondnum.value + " = " + result;
+    document.getElementById("result").innerHTML = result;
+    document.getElementById("mini-result").innerHTML =
+    firstnum.value + " + " + secondnum.value + " = " + result;
     firstnum.value = "";
     secondnum.value = "";
   };
 
   const div = () => {
     result = parseInt(firstnum.value) / parseInt(secondnum.value);
-    document.getElementById("result").innerHTML =
-      firstnum.value + " / " + secondnum.value + " = " + result;
+    document.getElementById("result").innerHTML = result;
+    document.getElementById("mini-result").innerHTML =
+    firstnum.value + " + " + secondnum.value + " = " + result;
     firstnum.value = "";
     secondnum.value = "";
   };
